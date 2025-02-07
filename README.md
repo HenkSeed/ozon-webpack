d83b935b7c7a28335c6af1953ebf6a68ceed0adb
HenkSeed <adaner@inbox.ru>
2025-02-07 18:51:35 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# JavaScript & Webpack для интернет-мазагина O-ZONE
---
+++ src/modules/second.js
@@ -0,0 +1,7 @@
+const second = () => {
+    const cartBtn = document.getElementById("cart")
+    console.log("cartBtn: ", cartBtn)
+    // console.log(cartBtn)
+}
+
+export default second
